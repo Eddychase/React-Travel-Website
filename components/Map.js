41d4d4f6f0057c0b068cb8487ca2f30f4cb7334b
Eddy Chase <eddykanyz.ek@gmail.com>
2022-01-05 21:29:59 +0300

    map view and component

---
+++ components/Map.js
@@ -0,0 +1,25 @@
+import ReactMapGL from 'react-map-gl';
+import { useState } from 'react'
+
+function Map() {
+
+    const [viewport, setViewport] = useState({
+        width: "100%",
+        height: "100%",
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+    })
+    return (
+        <ReactMapGL
+            mapStyle="mapbox://styles/eddychase/cky1v640x0tef14n8cfhgcpq6"
+            mapboxApiAccessToken={process.env.mapbox_key}
+            {...viewport}
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        >
+
+        </ReactMapGL>
+    )
+}
+
+export default Map
--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ['links.papareact.com']
     },
+     env: {
+        mapbox_key: 'pk.eyJ1IjoiZWRkeWNoYXNlIiwiYSI6ImNrdXdrNmFyeTBnczMyb21ydjIxN2czZmkifQ.avUyv_yZLvexIwBL8kobAw'
+    }
 };
