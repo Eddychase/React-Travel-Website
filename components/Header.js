aecef5fc7b27272417193ce58bb651fa63eef3d5
Eddy Chase <eddykanyz.ek@gmail.com>
2022-01-05 20:54:08 +0300

    site icon as homepage router button

--- components/Header.js
+++ components/Header.js
@@ -42,7 +42,8 @@ function Header({placeholder}) {
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-gray-200 shadow-md p-5
         md:px-10">
-        <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <div onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto">
             <Image
                     src="/pixels6.png"
                     layout="fill"
